193f4b995924f12522d54a1aef5d9470609a89cc
Apollo <developer.apollo.mail@gmail.com>
2021-11-25 12:52:25 -0300

    Layout complete

--- lib/JokenPo.dart
+++ lib/JokenPo.dart
@@ -6,6 +6,9 @@ class JokenPo extends StatefulWidget {
 }
 
 class _JokenPoState extends State<JokenPo> {
+  String _result_message = "Selecione uma jogada para começar";
+  String _computer_choice_image = "images/padrao.png";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,13 +20,36 @@ class _JokenPoState extends State<JokenPo> {
           child: Column(
             children: [
               Column(
-
+                children: [
+                  Text("Jogada do computador: "),
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(1)),
+                    child: Image(image: AssetImage(_computer_choice_image)),
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
               ),
-              Text(""),
+              Text(_result_message),
               Row(
-
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(1)),
+                    child: Image(image: AssetImage("images/papel.png")),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(1)),
+                    child: Image(image: AssetImage("images/pedra.png")),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(1)),
+                    child: Image(image: AssetImage("images/tesoura.png")),
+                  ),
+                ],
               )
             ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
           ),
         ),
       ),

0acf38fb7831eb612bec681ee34f65b70c2caba7
Apollo <developer.apollo.mail@gmail.com>
2021-11-25 12:39:39 -0300

    Added jokenpo stateful widget class file

---
+++ lib/JokenPo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class JokenPo extends StatefulWidget {
+  @override
+  _JokenPoState createState() => _JokenPoState();
+}
+
+class _JokenPoState extends State<JokenPo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
